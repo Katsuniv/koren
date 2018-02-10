512e4fece2a363f80b45f0629a72da2c317bb9f3
VItaly <katsuramiller@gmail.com>
2018-02-10 14:08:33 +0300

    Initial commit

---
+++ chetnechet/chetnechet/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	string vvod="";
+	string vvodp="";
+	ifstream fin("vvod.txt");
+	while (fin)
+	{
+		vvodp += vvod;
+		fin >> vvod;
+	}
+
+	cout << vvodp << endl;
+
+	int i = 0;
+	int chet = 0;
+	int nechet = 0;
+	while (vvodp[i] != NULL)
+	{
+		i++;
+		if (vvodp[i] == '0' || vvodp[i] == '2' || vvodp[i] == '4' || vvodp[i] == '6' || vvodp[i] == '8') chet++;
+		if (vvodp[i] == '1' || vvodp[i] == '3' || vvodp[i] == '5' || vvodp[i] == '7' || vvodp[i] == '9') nechet++;
+	}
+
+	cout << "„исло чЄтных чисел: " << chet << endl << "„исло нечЄтных чисел: " << nechet << endl;
+
+	system("pause");
+}
