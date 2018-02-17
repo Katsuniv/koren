0ca13bc047a63dcd90b7da15cef1a451e69aabff
Vitaly <katsuramiller@gmail.com>
2018-02-17 13:55:08 +0300

    initial commit

---
+++ koren/koren/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double R = 0;
+	double R1 = 0;
+	double R2 = 0;
+	for (int i = 2; i <= 98; i += 3)
+	{
+		R = sqrt(i);
+		R1 = i + 3;
+		R1 += R;
+		R2 = sqrt(R1);
+	}
+
+	cout << R2 << endl;
+
+	system("pause");
+}
